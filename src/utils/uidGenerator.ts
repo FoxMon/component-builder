70a2d5b7a96615e9f1ce8fd9b79d3208bb7b8b3b
foxmon <jjockrod@naver.com>
2023-10-04 22:56:33 +0900

    uid generator

--- src/utils/uidGenerator.ts
+++ src/utils/uidGenerator.ts
@@ -1,4 +1,9 @@
-export const uidGenerator = () => {
+/**
+ * 정말 단순히 유니크한 UID를 만들기 위한 메소드
+ *
+ * @returns {string}
+ */
+export const uidGenerator = (): string => {
   return `component-${(
     Date.now().toString(36) +
     ":" +
