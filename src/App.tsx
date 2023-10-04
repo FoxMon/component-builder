1fc43c276dfb213dec50b35170a387f7cd7a4161
foxmon <jjockrod@naver.com>
2023-10-04 22:56:18 +0900

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { Builder } from "./core/builder/builder";
+
 function App() {
+  const builder: Builder = new Builder();
+
+  const parentUid: string = builder.addChildNode("Box", "root", null);
+  builder.addChildNode("Input", parentUid, "Box");
+
+  console.log(builder);
+
   return <div>Hello</div>;
 }
 
