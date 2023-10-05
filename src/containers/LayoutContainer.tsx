08139d833b7255e6d61a1b3849a01eb1321029fb
foxmon <jjockrod@naver.com>
2023-10-05 16:23:45 +0900

    layout container

--- src/containers/LayoutContainer.tsx
+++ src/containers/LayoutContainer.tsx
@@ -1,63 +1,20 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import {
-  styled,
-  useTheme,
-  Box,
-  AppBar,
-  Toolbar,
-  Theme,
-  CssBaseline,
-} from "@mui/material";
+import { styled, Box, AppBar, Toolbar, CssBaseline } from "@mui/material";
 
 // project
 import { Header } from "@/layout/header/Header";
-import { Sidebar } from "@/layout/sidebar/Sidebar";
-
-interface ContentProps {
-  theme: Theme;
-  open: boolean;
-}
-
-const DRAWER_WIDTH: number = 260;
 
 // styles
 const Content = styled("main", {
   shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }: ContentProps) => ({
+})(() => ({
   borderBottomLeftRadius: 0,
   borderBottomRightRadius: 0,
   background: "#f8fafc",
   marginTop: "66.72px",
-  marginRight: "10px",
-  borderRadius: "8px",
-  transition: theme.transitions.create(
-    "margin",
-    open
-      ? {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }
-      : {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        },
-  ),
-  [theme.breakpoints.up("md")]: {
-    marginLeft: open ? "10px" : -DRAWER_WIDTH + 10,
-    width: "100%",
-  },
 }));
 
 export const LayoutContainer = () => {
-  const [isLeftDrawerOpen, setIsLeftDrawerOpen] = useState<boolean>(true);
-
-  const theme = useTheme();
-
-  const handleLeftDrawerChange = () => {
-    setIsLeftDrawerOpen((prev: boolean) => !prev);
-  };
-
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -67,21 +24,14 @@ export const LayoutContainer = () => {
         color="inherit"
         elevation={0}
         sx={{
-          bgcolor: theme.palette.background.default,
-          transition: isLeftDrawerOpen
-            ? theme.transitions.create("width")
-            : "none",
+          bgcolor: "#1a202c",
         }}
       >
         <Toolbar>
-          <Header onLeftDrawerChangeEvent={handleLeftDrawerChange} />
+          <Header />
         </Toolbar>
       </AppBar>
-      <Sidebar
-        isLeftDrawerOpen={isLeftDrawerOpen}
-        onLeftDrawerChangeEvent={handleLeftDrawerChange}
-      />
-      <Content theme={theme} open={isLeftDrawerOpen}>
+      <Content>
         <Outlet />
       </Content>
     </Box>
