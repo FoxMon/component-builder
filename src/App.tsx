3f42f43a3be7d44a961275240270bca1576139cf
foxmon <jjockrod@naver.com>
2023-10-05 15:30:04 +0900

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Agent } from "./core/builder/agent";
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+} from "@mui/material";
+
+// assets
+import { themePalette } from "./assets/theme/themePalette";
+
+// project
+import { ScrollArea } from "./layout/ScrollArea";
+import { Routes } from "./routes";
 
 function App() {
-  console.log(Agent.Form("root"));
-
-  return <div>Hello</div>;
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themePalette}>
+        <CssBaseline />
+        <ScrollArea>
+          <Routes />
+        </ScrollArea>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
 export default App;
