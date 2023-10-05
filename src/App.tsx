e55d756b9e34a6135664bcbd78f66b6372cbf379
foxmon <jjockrod@naver.com>
2023-10-05 16:23:49 +0900

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import {
-  CssBaseline,
-  StyledEngineProvider,
-  ThemeProvider,
-} from "@mui/material";
-
-// assets
-import { themePalette } from "./assets/theme/themePalette";
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
 
 // project
 import { AppErrorBoundary } from "./components/errorBoundaries/AppErrorBoundary";
@@ -15,14 +8,12 @@ import { Routes } from "./routes";
 function App() {
   return (
     <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={themePalette}>
-        <CssBaseline />
-        <AppErrorBoundary>
-          <ScrollArea>
-            <Routes />
-          </ScrollArea>
-        </AppErrorBoundary>
-      </ThemeProvider>
+      <CssBaseline />
+      <AppErrorBoundary>
+        <ScrollArea>
+          <Routes />
+        </ScrollArea>
+      </AppErrorBoundary>
     </StyledEngineProvider>
   );
 }
