a1f6c9f03f3e15c0cafed96f81ec410cd64c2ee0
foxmon <jjockrod@naver.com>
2023-10-05 15:51:57 +0900

    Error Boundary

---
+++ src/components/errorBoundaries/AppErrorBoundary.tsx
@@ -0,0 +1,79 @@
+import { Component } from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+import { logging } from "@/utils/logger";
+import type { Children } from "@/types/component";
+
+interface AppErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class AppErrorBoundary extends Component<
+  Children,
+  AppErrorBoundaryState
+> {
+  state: AppErrorBoundaryState;
+
+  constructor(props: Children) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  /**
+   * @override
+   *
+   * @param {unknown} error
+   * @returns {AppErrorBoundaryState}
+   */
+  static getDerivedStateFromError(error: unknown): AppErrorBoundaryState {
+    logging(error);
+
+    return {
+      hasError: true,
+    };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <Box
+          sx={{
+            backgroundImage:
+              "linear-gradient(to right, #d9e2e9 1px, transparent 1px),linear-gradient(to bottom, #d9e2e9 1px, transparent 1px);",
+            backgroundSize: "20px 20px",
+            background: "#edf2f6",
+            display: "flex",
+            flex: 1,
+            height: "100vh",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Stack
+            alignItems="center"
+            direction="column"
+            spacing={8}
+            bgcolor="white"
+            px={6}
+            py={6}
+            boxShadow={1}
+          >
+            <h1>Oops...</h1>
+            <Typography>Something went wrong! Clear and refresh.</Typography>
+            <Button
+              size="small"
+              variant="outlined"
+              onClick={() => window.location.reload()}
+            >
+              Clear & reload
+            </Button>
+          </Stack>
+        </Box>
+      );
+    }
+
+    return this.props.children;
+  }
+}
