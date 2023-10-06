67eade3bb43e2af88b04640ea2daf549123f0416
foxmon <jjockrod@naver.com>
2023-10-06 16:41:53 +0900

    sidebar item

--- src/layout/sidebar/SidebarItem.tsx
+++ src/layout/sidebar/SidebarItem.tsx
@@ -1,13 +1,37 @@
+import { useDrag } from "react-dnd";
 import { Box, Typography } from "@mui/material";
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 
-export const SidebarItem = () => {
+interface SidebarItem {
+  name: string;
+  componentType: string;
+  rootComponentType: string;
+  isChildComponent: boolean;
+}
+
+export const SidebarItem = ({
+  name,
+  componentType,
+  rootComponentType,
+  isChildComponent,
+}: SidebarItem) => {
+  const [_, drag] = useDrag({
+    type: componentType,
+    item: {
+      id: componentType,
+      type: componentType,
+      rootComponentType: rootComponentType,
+    },
+  });
+
   return (
     <Box
+      ref={drag}
       sx={{
         boxSizing: "border-box",
         transition: "margin 200ms",
         my: 1,
+        ml: isChildComponent ? 4 : 0,
         borderRadius: "8px",
         p: 1,
         display: "flex",
@@ -18,7 +42,7 @@ export const SidebarItem = () => {
     >
       <DragIndicatorIcon sx={{ mr: 2, fontSize: "1.125rem" }} />
       <Typography variant="body1" sx={{ fontSize: "0.875rem" }}>
-        Box
+        {name}
       </Typography>
     </Box>
   );
