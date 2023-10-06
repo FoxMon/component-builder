4d501ef74db1245b7d76c4e0ac2e28f9a1a7bc34
foxmon <jjockrod@naver.com>
2023-10-06 16:41:44 +0900

    component

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -1,7 +1,13 @@
 import type { CommonComponentType } from "@/types/component";
 
+/**
+ * 현재 갖고 있는 Component들의 집합
+ */
 export const components: CommonComponentType[] = ["Box", "Form", "Input"];
 
+/**
+ * 부모가 될 수 있는 Root component 정의
+ */
 export const possibleRootComponents: CommonComponentType[] = components.filter(
   (name: CommonComponentType) => ["Box", "Form"].includes(name),
 );
