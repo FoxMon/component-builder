389b6e77618b0144b41bdff03672ca6a0dcd9396
foxmon <jjockrod@naver.com>
2023-10-06 16:42:25 +0900

    main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,25 @@
+import { Box } from "@mui/material";
+
+// proejct
+import { ComponentEditor } from "@/components/editor/ComponentEditor";
+
 // core
 import { Agent } from "@/core/builder/agent";
 
 export const Main = () => {
   console.log(Agent.Form("root"));
 
-  return <div>Main</div>;
+  return (
+    <Box
+      sx={{
+        backgroundImage:
+          "linear-gradient(to right, #d9e2e9 1px, transparent 1px), linear-gradient(to bottom, #d9e2e9 1px, transparent 1px);",
+        backgroundSize: "20px 20px",
+        bgColor: "#edf2f6",
+        minHeight: "100vh",
+      }}
+    >
+      <ComponentEditor />
+    </Box>
+  );
 };
