8697a3d6fbea5d8a2c770051a7e0d121fca483dd
foxmon <jjockrod@naver.com>
2023-10-06 13:08:02 +0900

    layout container

--- src/containers/LayoutContainer.tsx
+++ src/containers/LayoutContainer.tsx
@@ -3,37 +3,44 @@ import { styled, Box, AppBar, Toolbar, CssBaseline } from "@mui/material";
 
 // project
 import { Header } from "@/layout/header/Header";
+import { Sidebar } from "@/layout/sidebar/Sidebar";
+import { Observer } from "@/components/observer/Observer";
 
 // styles
-const Content = styled("main", {
-  shouldForwardProp: (prop) => prop !== "open",
-})(() => ({
+const Content = styled("main")(() => ({
   borderBottomLeftRadius: 0,
   borderBottomRightRadius: 0,
-  background: "#f8fafc",
-  marginTop: "66.72px",
+  width: "100%",
+  backgroundImage:
+    "linear-gradient(to right, #d9e2e9 1px, transparent 1px), linear-gradient(to bottom, #d9e2e9 1px, transparent 1px);",
+  backgroundSize: "20px 20px",
+  bgColor: "#edf2f6",
 }));
 
 export const LayoutContainer = () => {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box>
       <CssBaseline />
       <AppBar
-        enableColorOnDark
-        position="fixed"
+        position="static"
         color="inherit"
         elevation={0}
         sx={{
           bgcolor: "#1a202c",
+          height: "60px",
         }}
       >
         <Toolbar>
           <Header />
         </Toolbar>
       </AppBar>
-      <Content>
-        <Outlet />
-      </Content>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Content>
+          <Outlet />
+        </Content>
+        <Observer />
+      </Box>
     </Box>
   );
 };
