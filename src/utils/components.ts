d8b0494bfc7ea61ea09fb770a27f1d8d97e8b6d7
foxmon <jjockrod@naver.com>
2023-10-06 22:04:27 +0900

    components

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -1,4 +1,9 @@
+// util
+import { assert } from "./assert";
+
+// type
 import type { CommonComponentType } from "@/types/component";
+import type { Nullable } from "@/types/common";
 
 /**
  * 현재 갖고 있는 Component들의 집합
@@ -6,8 +11,20 @@ import type { CommonComponentType } from "@/types/component";
 export const components: CommonComponentType[] = ["Box", "Form", "Input"];
 
 /**
- * Drag가 될 수 있는 component 정의
+ * Creator로 build할 수 있는 component인지 유효성 검사
+ * 만약 component-builder project가 가지고 있지 않은 component라면
+ * Creator가 만들 수 없는 component이다.
+ *
+ * @param {string} componentType
+ * @returns {boolean}
  */
-export const possibleDragComponents: CommonComponentType[] = components.filter(
-  (name: CommonComponentType) => ["Box", "Form", "Input"].includes(name),
-);
+export const checkComponent = (componentType: string): boolean => {
+  const foundComponent: Nullable<string[]> = components.filter(
+    (c: string) => c === componentType,
+  );
+
+  // 유효한 component의 type이 아니라면 반드시 Error를 나타낸다.
+  assert(foundComponent.length > 0, "Cannot found this component");
+
+  return true;
+};
