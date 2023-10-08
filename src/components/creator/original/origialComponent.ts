45ecacc0f85b00ea9b8ad405c0b30b5c8938b3ce
foxmon <jjockrod@naver.com>
2023-10-08 20:14:16 +0900

    original component

---
+++ src/components/creator/original/origialComponent.ts
@@ -0,0 +1,15 @@
+/**
+ * Original component mapper type
+ */
+interface OriginalComponents {
+  [key: string]: string;
+}
+
+/**
+ * Original component mapper
+ */
+export const OriginalComponent: OriginalComponents = {
+  Input: "OriginalInput",
+  Box: "OriginalBox",
+  Form: "OriginalForm",
+};
