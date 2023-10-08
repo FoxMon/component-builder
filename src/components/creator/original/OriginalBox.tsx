6c253a74c496e6a1d7f15d75f39a88418f906740
foxmon <jjockrod@naver.com>
2023-10-08 20:13:55 +0900

    original box

---
+++ src/components/creator/original/OriginalBox.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+
+// type
+import { Children } from "@/types/component";
+
+export const OriginalBox = ({ children }: Children) => {
+  return <Box>{children}</Box>;
+};
