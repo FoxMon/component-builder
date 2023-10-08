a52402834c75a21cf051854f37cd8d4438b438d7
foxmon <jjockrod@naver.com>
2023-10-08 20:42:15 +0900

    target

--- src/core/componeents/target.ts
+++ src/core/componeents/target.ts
@@ -14,23 +14,10 @@ export interface TargetComponent {
   components: Components;
 }
 
-// 굳이 default가 있어야 하나?
 // Dragging을 하지 않았다면 충분히 null일 가능성이 존재한다.
-// 따라서 Nullable 타입으로 지정해야 하지 않을까...
-export const targetComponent = atom<TargetComponent>({
+export const targetComponent = atom<Nullable<TargetComponent>>({
   key: "Selected:Target:Component",
-  default: {
-    selectedComponentUid: "",
-    components: {
-      [""]: {
-        uid: "",
-        parent: "",
-        children: [],
-        commonComponentType: "Box",
-        rootCommonComponentType: "Box",
-      },
-    },
-  },
+  default: null,
 });
 
 // Editor에 배치된 Components
