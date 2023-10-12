f8071d67408ac013f19a679612c0daf74c1b9402
foxmon <jjockrod@naver.com>
2023-10-12 22:55:28 +0900

    Main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,19 +1,30 @@
+import { useContext, useMemo } from "react";
 import { Box } from "@mui/material";
 
 // proejct
 import { WithLayout } from "@/hoc/withLayout";
 import { ComponentEditor } from "@/components/editor/ComponentEditor";
+import { EditorBackgroundStateContext } from "@/components/contexts/editorContext";
 
 const WrappedComponentEditor = WithLayout(ComponentEditor);
 
 export const Main = () => {
+  const { isBackgroundMode } = useContext(EditorBackgroundStateContext);
+
+  const boxProps = useMemo(() => {
+    return isBackgroundMode
+      ? { bgColor: "#FFFFFF" }
+      : {
+          backgroundImage:
+            "linear-gradient(to right, #d9e2e9 1px, transparent 1px), linear-gradient(to bottom, #d9e2e9 1px, transparent 1px);",
+          backgroundSize: "20px 20px",
+        };
+  }, [isBackgroundMode]);
+
   return (
     <Box
       sx={{
-        backgroundImage:
-          "linear-gradient(to right, #d9e2e9 1px, transparent 1px), linear-gradient(to bottom, #d9e2e9 1px, transparent 1px);",
-        backgroundSize: "20px 20px",
-        bgColor: "#edf2f6",
+        ...boxProps,
         minHeight: "100vh",
       }}
     >
