8ea1dd8df09683202e67a5d643149ebe87144c32
foxmon <jjockrod@naver.com>
2023-10-13 15:14:35 +0900

    use drag drop target

---
+++ src/hooks/useDragTarget.ts
@@ -0,0 +1,26 @@
+import { useRef } from "react";
+import { useDrag } from "react-dnd";
+
+// type
+import type { ComponentBase } from "@/types/component";
+
+export const useDragTarget = (component: ComponentBase) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [_, drag] = useDrag({
+    type: component.commonComponentType,
+    item: {
+      cUid: component.uid,
+      commonComponentType: component.commonComponentType,
+      isMoved: true,
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging,
+    }),
+  });
+
+  return {
+    ref: drag(ref),
+    drag,
+  };
+};
