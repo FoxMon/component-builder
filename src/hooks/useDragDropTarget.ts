e47b6b01dd143b3cb81f5660a58e3c7abf9d92e9
foxmon <jjockrod@naver.com>
2023-10-13 15:14:47 +0900

    use drag drop target

--- src/hooks/useDragDropTarget.ts
+++ src/hooks/useDragDropTarget.ts
@@ -13,21 +13,14 @@ import { agentBuilder } from "@/core/builder/agent";
 
 // type
 import type { CommonComponentType, Components } from "@/types/component";
+import type { DragDropComponent } from "@/types/common";
 
 // utils
 import { components } from "@/utils/components";
 import { Nullable } from "@/types/common";
 
-interface DropComponentItem {
-  type: string;
-  cUid: string;
-  name: string;
-  componentType: string;
-  rootComponentType: string;
-  isMoved: string;
-  isChildCOmponent: boolean;
-}
-
+// Drag이후 Drop할 때의 동작할 Hook
+// 즉 Component를 떨궜을 때
 export const useDragDropTarget = (
   cUid: string,
   isPossible: boolean = true,
@@ -45,30 +38,75 @@ export const useDragDropTarget = (
     collect: (monitor) => ({
       isOver: monitor.isOver({ shallow: true }) && monitor.canDrop(),
     }),
-    drop: (item: DropComponentItem, monitor: DropTargetMonitor) => {
+    drop: (item: DragDropComponent, monitor: DropTargetMonitor) => {
       if (!monitor.isOver()) {
         // 놓여 졌을 때만 특정 동작을 수행하도록 한다.
         return;
       }
 
-      const generatedComponent: Components = agentBuilder[item.type](
-        item.rootComponentType,
-      ).components;
+      if (item.isMoved) {
+        if (!currentPlacedTargetComponent) {
+          // 당연히 배치된 Component가 없다면 여기 조건문을 통과 안하겠지만
+          // 그래도 혹시 모르니 예외처리를 진행하도록 한다.
+          return;
+        }
 
-      const componentObject: Components = {};
-      Object.keys(generatedComponent).forEach((uid: string) => {
-        componentObject[uid] = generatedComponent[uid];
-      });
+        const movedComponentTarget: Components = {};
+        Object.keys(currentPlacedTargetComponent).forEach((id: string) => {
+          if (id === item.cUid) {
+            // Update돼야 하는 Component의 정보 재배치
+            // 부모의 정보(Component ID)를 갱신한다.
+            movedComponentTarget[id] = {
+              uid: id,
+              parent: cUid,
+              children: currentPlacedTargetComponent[id].children,
+              commonComponentType:
+                currentPlacedTargetComponent[id].commonComponentType,
+              rootCommonComponentType:
+                currentPlacedTargetComponent[cUid].rootCommonComponentType,
+            };
+          } else if (id === cUid) {
+            // Update돼야 하는 Component의 정보 재배치
+            // 부모에게 자식의 정보(Component ID)를 넣어준다.
+            movedComponentTarget[cUid] = {
+              uid: cUid,
+              parent: currentPlacedTargetComponent[cUid].parent,
+              children: [
+                ...currentPlacedTargetComponent[cUid].children,
+                item.cUid,
+              ],
+              commonComponentType:
+                currentPlacedTargetComponent[cUid].commonComponentType,
+              rootCommonComponentType:
+                currentPlacedTargetComponent[cUid].rootCommonComponentType,
+            };
+          } else {
+            // 기존의 것들은 그대로 배치시킨다.
+            movedComponentTarget[id] = currentPlacedTargetComponent[id];
+          }
+        });
 
-      setTargetComponent({
-        selectedComponentUid: cUid,
-        components: componentObject,
-      });
+        setPlacedTargetComponent(movedComponentTarget);
+      } else {
+        const generatedComponent: Components = agentBuilder[item.type](
+          item.rootComponentType,
+        ).components;
 
-      setPlacedTargetComponent({
-        ...currentPlacedTargetComponent,
-        ...componentObject,
-      });
+        const componentObject: Components = {};
+        Object.keys(generatedComponent).forEach((uid: string) => {
+          componentObject[uid] = generatedComponent[uid];
+        });
+
+        setTargetComponent({
+          selectedComponentUid: cUid,
+          components: componentObject,
+        });
+
+        setPlacedTargetComponent({
+          ...currentPlacedTargetComponent,
+          ...componentObject,
+        });
+      }
     },
     canDrop: () => isPossible,
   });
