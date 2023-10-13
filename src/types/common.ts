d303beaa414797da64c2e84dce2a8e0f926240f3
foxmon <jjockrod@naver.com>
2023-10-13 15:13:47 +0900

    common

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,17 @@
+// type
+import type { CommonComponentType } from "./component";
+
 /**
  * Null일 가능성도 있는 Type
  */
 export type Nullable<Type> = Type | null | undefined;
+
+export interface DragDropComponent {
+  type: string;
+  cUid: string;
+  name: string;
+  componentType: CommonComponentType;
+  rootComponentType: string;
+  isMoved: string;
+  isChildCOmponent: boolean;
+}
