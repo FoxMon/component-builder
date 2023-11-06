03d64a45da2a48b741440d3a092ce3a53e9b7195
foxmon <jjockrod@naver.com>
2023-11-06 14:24:27 +0900

    editor context

--- src/components/contexts/EditorContext.tsx
+++ src/components/contexts/EditorContext.tsx
@@ -5,29 +5,45 @@ import type { Children } from "@/types/component";
 
 interface EditorContextState {
   isBackgroundMode: boolean;
+  isCodeMode: boolean;
   updateBackgroundModeState: () => void;
+  updateCodeModeState: () => void;
 }
 
 // 모눈종이 => false
 // 흰색 => true
 export const EditorBackgroundStateContext = createContext<EditorContextState>({
   isBackgroundMode: false,
+  isCodeMode: true,
   updateBackgroundModeState: () => {},
+  updateCodeModeState: () => {},
 });
 
 export const EditorContext = ({ children }: Children) => {
   const [isBackground, setIsBackground] = useState<boolean>(false);
+  const [isCodeMode, setIsCodeMode] = useState<boolean>(true);
 
   const handleBackgroundModeStateChange = useCallback(() => {
     setIsBackground((prev: boolean) => !prev);
   }, []);
 
+  const handleCodeModeStateChange = useCallback(() => {
+    setIsCodeMode((prev: boolean) => !prev);
+  }, []);
+
   const editorBackgroundValues: EditorContextState = useMemo(() => {
     return {
       isBackgroundMode: isBackground,
+      isCodeMode: isCodeMode,
       updateBackgroundModeState: handleBackgroundModeStateChange,
+      updateCodeModeState: handleCodeModeStateChange,
     };
-  }, [isBackground, handleBackgroundModeStateChange]);
+  }, [
+    isBackground,
+    isCodeMode,
+    handleBackgroundModeStateChange,
+    handleCodeModeStateChange,
+  ]);
 
   return (
     <EditorBackgroundStateContext.Provider value={editorBackgroundValues}>
